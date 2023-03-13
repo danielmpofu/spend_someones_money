5b5cf041169b4a83743cd28d60c623c9ca4b0f9f
Daniel T Mpofu <danielmpofu@hotmail.com>
2023-03-13 18:25:17 +0200

    Basic  project setup
    Added vuex, added vuetify and bootstrap

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
 import { createApp } from 'vue'
+import './assets/app_style.css';
+import './assets/bootstrap-grid_min.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// Vuetify
+
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
+createApp(App).use(vuetify).mount('#app')
